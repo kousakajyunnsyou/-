a31626e21ccca6c270fec5ce4458461714d40dd5
kousakajyunnsyou <80510162+kousakajyunnsyou@users.noreply.github.com>
2021-03-23 09:43:11 +0800

    Add files via upload
    始まりだ！

---
+++ fan_recoder.co/fan_recoder.co/Model/ContentsTableSection.swift
@@ -0,0 +1,51 @@
+//
+//  ContentsTableSection.swift
+//  fan_recoder.co
+//
+//  Created by yons on 2021/2/2.
+//  Copyright © 2021 yons. All rights reserved.
+//  目录画面tableView 的数据模型
+
+import UIKit
+import Foundation
+import RxDataSources
+import RxSwift
+import RxCocoa
+
+struct ContentsTableSection {
+    
+    var header: String
+    var items: [Item]
+
+}
+
+extension ContentsTableSection : AnimatableSectionModelType {
+
+    var identity: String {
+        return header
+    }
+
+    typealias Item = TableDataModel
+
+    init(original: ContentsTableSection, items: [Item]) {
+        self = original
+        self.items = items
+    }
+}
+
+struct TableDataModel: Equatable {
+    let name: String
+    let toView: String
+    
+    init(name:String,toView:String) {
+        self.name = name
+        self.toView = toView
+    }
+}
+
+extension TableDataModel: IdentifiableType {
+    typealias Identity = String
+    var identity: Identity {
+        return name
+    }
+}
---
+++ fan_recoder.co/fan_recoder.co/ViewCon/TestNavigationController2.swift
@@ -0,0 +1,19 @@
+//
+//  TestView2.swift
+//  fan_recoder.co
+//
+//  Created by yons on 2021/1/19.
+//  Copyright © 2021 yons. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+
+class TestNavigationController2: UINavigationController {
+    
+    override func viewDidLoad() {
+        self.navigationController?.navigationBar.backgroundColor = UIColor.green
+    }
+}
